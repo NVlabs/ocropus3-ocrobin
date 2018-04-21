4701829e89c2243983d0375f32ccd4542b6df8b6
Tom <tmbdev@gmail.com>
2018-04-21 15:29:55 -0700

    Updated installer.

--- setup.py
+++ setup.py
@@ -16,6 +16,12 @@ from distutils.core import setup  # , Extension, Command
 assert sys.version_info[0] == 2 and sys.version_info[1] >= 7,\
     "requires Python version 2.7 or later, but not Python 3.x"
 
+remote = "https://storage.googleapis.com/tmb-models/"
+models = ["bin-000000046-005393.pt"]
+for model in models:
+    if not os.path.exists(model):
+        print("downloading", remote+model)
+        urllib.URLopener().retrieve(remote+model, model)
 
 scripts = """
 ocrobin-train
@@ -29,4 +35,5 @@ setup(
     description="Document image binarization.",
     packages=["ocrobin"],
     scripts=scripts,
+    data_files = [("share/ocrobin", models)]
 )
